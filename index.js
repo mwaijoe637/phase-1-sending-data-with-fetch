34944b1de7fd63112ded291691eba24abae3cd91
joe <brakejoe9@gmail.com>
2024-04-17 09:51:20 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name, email) {
+    const formData = {
+      name: name,
+      email: email
+    };
+  
+    const config = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch('http://localhost:3000/users', config)
+      .then(response => response.json())
+      .then(data => {
+        const id = data.id;
+        document.body.innerHTML += id;
+      })
+      .catch(error => {
+        document.body.innerHTML += error.message;
+      });
+  }
+  
